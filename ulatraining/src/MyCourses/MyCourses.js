5bb82de29cdfe69a75f8d1c4f667ed9723414d09
Sami Habib <46608419+samih36@users.noreply.github.com>
2021-10-31 16:26:05 -0400

    progress bar working

--- ulatraining/src/MyCourses/MyCourses.js
+++ ulatraining/src/MyCourses/MyCourses.js
@@ -10,23 +10,34 @@ export default function MyCourses(props) {
     const { currentUser } = useAuth();
     const database = props.database;
     const [courses, setCourses] = useState({});
-    const [completion, setCompletion] = useState(false);
-    const [passPercentage, setPassPercentage] = useState({});
+    const [progress, setProgress] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    const progressNotLoaded = () => {
+        if (Object.keys(progress).length === 0) {
+            return true;
+        } else {
+            for (let i = 0; i < Object.keys(progress).length; i++) {
+                if (isNaN(progress[Object.keys(progress)[i]])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     useEffect(() => {
-        database.ref('users').child(currentUser.uid).child('courses').on('value', snapshot => {
-            console.log(snapshot.val());
-            if (snapshot.exists()) {
-                setCourses(snapshot.val());
+        if (progressNotLoaded()) {
+            database.ref('users').child(currentUser.uid).child('courses').on('value', snapshot => {
+                if (snapshot.exists()) {
+                    setCourses(snapshot.val());
 
-            }
+                }
+            });
             for(let courseID in courses) {
-                let scores = {};
+                let scores = courses[courseID].modules;
                 let percentages = {};
-                database.ref('users').child(currentUser.uid).child('courses').child(courseID).on('value', snapshot => {
-                    console.log(snapshot.val().modules);
-                    scores = snapshot.val().modules;
-                })
+                let passCount = 0;
 
         
                 database.ref('courses').child(courseID).child('modules').on('value', snapshot => {
@@ -35,38 +46,48 @@ export default function MyCourses(props) {
                         quizzes[key].type === 'quiz' ? percentages[key] = quizzes[key].passPercentage : percentages[key] = 1;
                     }
                 })
-                console.log('quiz');
-                console.log(scores)
-                console.log(percentages)
+
+                for(let module in scores) { 
+                    if (scores[module] >= percentages[module]) {
+                        passCount++;
+                    }
+                }
+                progress[courseID] = Math.round(((passCount / Object.keys(percentages).length) * 100));
             }
-        });
+        setLoading(false)
+        }
+    }, [courses]);
 
-    }, []);
+    if (loading) {
+        return null;
+    } else {
+        return (
+            <div className="container">
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={7}>
+                            <div className="coursesHeader">courses</div>
 
-    return (
-        <div className="container">
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={1}>
-                    <Grid item xs={7}>
-                        <div className="coursesHeader">courses</div>
+                            {Object.keys(courses).map((cid) => {
+                                return (<div className="course" key={cid} onClick={event => window.location.href=`/course/${cid}`}>{courses[cid].name}</div>)
+                            })}
+                        </Grid>
+                        <Grid item xs={3}>
+                            <div className="coursesHeader">Progress</div>
+                            {Object.keys(courses).map((cid) => {
+                                return progress[cid] === 100 ?
+                                <div key={cid} className="completedCourse">{progress[cid]}% </div> :
+                                <div key={cid} className="course">{progress[cid]}% </div>
+                            })}
+                        </Grid>
+                        <Grid item xs={2}>
+                            <div className="addClass" onClick={event => window.location.href='add-course'}>add course</div>
+                        </Grid>
 
-                        {Object.keys(courses).map((cid) => {
-                            return (<div className="course" key={cid} onClick={event => window.location.href=`/course/${cid}`}>{courses[cid].name}</div>)
-                        })}
                     </Grid>
-                    <Grid item xs={3}>
-                        <div className="coursesHeader">Progress</div>
-                        {Object.keys(courses).map((cid) => {
-                            return (<div key={cid} className="course">0%</div>)
-                        })}
-                    </Grid>
-                    <Grid item xs={2}>
-                        <div className="addClass" onClick={event => window.location.href='add-course'}>add course</div>
-                    </Grid>
-
-                </Grid>
-            </Box>
-        </div>
-    );
+                </Box>
+            </div>
+        );
+    }
 
 }
