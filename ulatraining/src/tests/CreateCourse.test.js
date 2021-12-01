8649e9178af8783d88199a0fee81cf53298d9a56
Sami Habib <46608419+samih36@users.noreply.github.com>
2021-12-01 17:55:33 -0500

    testss

--- ulatraining/src/tests/CreateCourse.test.js
+++ ulatraining/src/tests/CreateCourse.test.js
@@ -10,6 +10,60 @@ import testing from '../testingFirebaseConfig.js'
 
 configure({ adapter: new Adapter() });
 
+let fakeCoursesSnapshot = {
+    mfklsdjfhfjdk: {
+        name: "COMP 123",
+        professor: "dfhsakjfkladjfl",
+        modules: {
+            safjdkjfdsljzzz: {
+                name: 'mod1',
+                type: 'text',
+                content: 'this is an example text module'
+            }, 
+            fjudfsahdsadkl: {
+                name: 'mod2',
+                type: 'text',
+                content: 'here is the second text module'
+            }
+        }
+    }
+}
+
+let fakeUserCourses = {
+    mfklsdjfhfjdk: {
+        name: "COMP 123",
+        professor: 'dfhsakjfkladjfl',
+        modules: {
+            safjdkjfdsljzzz: 100,
+            fjudfsahdsadkl: 0
+
+        }
+    }
+}
+
+let user = {
+    currentUser: {
+        uid: 'dfhsakjfkladjfl',
+        role: "professor"
+    }
+}
+
+let testUsers = {
+    teststudent: {
+        courses: fakeUserCourses,
+        firstName: "Test",
+        lastName: "Student",
+        onyen: "teststudent",
+        role: "student"
+    },
+    dfhsakjfkladjfl: {
+        firstName: "professor",
+        lastName: "smart",
+        role: "professor"
+    }
+    
+}
+
 describe("CreateCourse creates a course", () => {
     it("should render a course creation page", () => {
         let user = {
@@ -19,10 +73,14 @@ describe("CreateCourse creates a course", () => {
         }
         jest.spyOn(AuthContext, 'useAuth').mockImplementation(() => user);
         let fakeDatabase = testing.database();
+        fakeDatabase.ref('users').set(testUsers);
+        fakeDatabase.ref('courses').set(fakeCoursesSnapshot);
+        
 
         const wrapper = mount(<CreateCourse database={fakeDatabase} />)
         expect(getInnerHTML(wrapper.find('.coursesHeader').first())).toEqual('Create a Course')
         expect(getInnerHTML(wrapper.find('.courseTitleLabel').first())).toEqual('Course Title:')
+        wrapper.find('.advanceButton').last().simulate('click');
         expect((wrapper.find('#courseTitleInput').first())).toBeTruthy()
     })
 })
